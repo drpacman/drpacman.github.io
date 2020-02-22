f4ba51506238fb9bf86b70f49f66f7b32badd8b7
Paul Caporn <paul.caporn@bbc.co.uk>
2020-02-22 22:20:01 +0000

    Update description

--- sphero/extension.js
+++ sphero/extension.js
@@ -29,5 +29,5 @@
     };
 
     // Register the extension
-    ScratchExtensions.register('Weather extension', descriptor, ext);
+    ScratchExtensions.register('Sphero connector extension', descriptor, ext);
 })({});
